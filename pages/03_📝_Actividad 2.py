80c40531e2c2e71d336140eafd0b0fd13ff758d9
Esteban Villalobos <egvillalobos@uc.cl>
2024-06-25 11:37:39 +0200

    Fix: Button args

--- pages/03_📝_Actividad 2.py
+++ pages/03_📝_Actividad 2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from chatbot_helper import summarize_responses
 
-activity_id = 'Actividad_2'
+activity_id = ('Actividad_2')
 st.button("Resumir respuestas", on_click=summarize_responses, args=activity_id)
 
